6135ed327084d0107a1551f57bf4538894ac4359
guledabdilatif <guledabdilatif@gmail.com>
2025-08-18 12:22:38 -0700

    login page

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="container-fluid vh-100 d-flex align-items-center justify-content-center bg-light">
+      <h1 className="fw-bold text-success">Welcome to the Dashboard 🎉</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
